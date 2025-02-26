b3dab1d8b1cd40f894059158fe3a9ebe3610ac86
Golde34 <nguyendongducviet2001@gmail.com>
2025-02-26 09:00:28 +0700

    add wait group to make kafka and crons into 2 independent threads in crons job

--- gaia_cron_jobs/cmd/main.go
+++ gaia_cron_jobs/cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"gaia_cron_jobs/internal/kafka"
 	"gaia_cron_jobs/ui"
 	"log"
+	"sync"
 )
 
 func main() {
@@ -21,11 +22,22 @@ func main() {
 	}
 	consumerGroupHandler := kafka.NewConsumerGroupHandler(kafkaCfg.Name, handlers)
 
+	// Use WaitGroup to handle goroutines
+	var wg sync.WaitGroup
+	wg.Add(2)
+
 	go func() {
+		defer wg.Done()
+		log.Println("Starting Kafka consumer...")
 		kafka.ConsumerGroup(kafkaCfg.BootstrapServers, kafkaCfg.Topics, kafkaCfg.GroupID, consumerGroupHandler)
 	}()
 
 	// Start scheduler
-	log.Println("Dynamic cron job service started...")
-	cron.ExecuteJob()
+	go func() {
+		defer wg.Done()
+		log.Println("Dynamic cron job service started...")
+		cron.ExecuteJob()
+	}()
+
+	wg.Wait()
 }
