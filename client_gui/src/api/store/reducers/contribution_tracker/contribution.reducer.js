ac38d0da0f46c95ce5112b3fa06d3098bf986db8
Golde34 <nguyendongducviet2001@gmail.com>
2025-03-01 19:18:34 +0700

    add compare commits api in CLG

--- client_gui/src/api/store/reducers/contribution_tracker/contribution.reducer.js
+++ client_gui/src/api/store/reducers/contribution_tracker/contribution.reducer.js
@@ -1,4 +1,6 @@
-import { USER_CONTRIBUTIONS_FAILURE, USER_CONTRIBUTIONS_REQUEST, USER_CONTRIBUTIONS_SUCCESS } from "../../constants/contribution_tracker/contribution.constants";
+import { COMPARE_COMMTIS_FAILURE, COMPARE_COMMTIS_REQUEST, COMPARE_COMMTIS_SUCCESS, 
+    USER_CONTRIBUTIONS_FAILURE, USER_CONTRIBUTIONS_REQUEST, USER_CONTRIBUTIONS_SUCCESS 
+} from "../../constants/contribution_tracker/contribution.constants";
 
 export const getUserContributionsReducer = (
     state = { loading: true }, action) => {
@@ -8,6 +10,20 @@ export const getUserContributionsReducer = (
         case USER_CONTRIBUTIONS_SUCCESS:
             return { loading: false, contributions: action.payload }
         case USER_CONTRIBUTIONS_FAILURE:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
+
+export const compareCommitsReducer = (
+    state = { loading: true }, action) => {
+    switch (action.type) {
+        case COMPARE_COMMTIS_REQUEST:
+            return { loading: true };
+        case COMPARE_COMMTIS_SUCCESS:
+            return { loading: false, commits: action.payload }
+        case COMPARE_COMMTIS_FAILURE:
             return { loading: false, error: action.payload };
         default:
             return state;
--- client_gui/src/components/subComponents/AreaChartComponent.jsx
+++ client_gui/src/components/subComponents/AreaChartComponent.jsx
@@ -3,159 +3,136 @@ import { Card, Title, AreaChart } from "@tremor/react";
 import { ContributionCalendar, createTheme } from 'react-contribution-calendar';
 import { useDispatch, useSelector } from "react-redux";
 import MessageBox from "./MessageBox";
-import { getUserContributions } from "../../api/store/actions/contribution_tracker/contribution.actions";
-
-const chartdata = [
-  {
-    date: "Jan 22",
-    SemiAnalysis: 2890,
-    "The Pragmatic Engineer": 2338,
-  },
-  {
-    date: "Feb 22",
-    SemiAnalysis: 2756,
-    "The Pragmatic Engineer": 2103,
-  },
-  {
-    date: "Mar 22",
-    SemiAnalysis: 3322,
-    "The Pragmatic Engineer": 2194,
-  },
-  {
-    date: "Apr 22",
-    SemiAnalysis: 3470,
-    "The Pragmatic Engineer": 2108,
-  },
-  {
-    date: "May 22",
-    SemiAnalysis: 3475,
-    "The Pragmatic Engineer": 1812,
-  },
-  {
-    date: "Jun 22",
-    SemiAnalysis: 3129,
-    "The Pragmatic Engineer": 1726,
-  },
-];
-
-const dataFormatter = (number) => {
-  return "$ " + Intl.NumberFormat("us").format(number).toString();
-};
+import { getCompareCommits, getUserContributions } from "../../api/store/actions/contribution_tracker/contribution.actions";
 
 const customTheme = createTheme({
-  level0: '#20263c',
-  level1: '#23525D',
-  level2: '#277D7E',
-  level3: '#2AA99E',
-  level4: '#2dd4bf',
+    level0: '#20263c',
+    level1: '#23525D',
+    level2: '#277D7E',
+    level3: '#2AA99E',
+    level4: '#2dd4bf',
 });
 
 const AreaChartComponent = () => {
-  const [calendarSize, setCalendarSize] = useState({
-    cx: 12,
-    cy: 12,
-    cr: 5,
-  });
+    const [calendarSize, setCalendarSize] = useState({
+        cx: 12,
+        cy: 12,
+        cr: 5,
+    });
 
-  useEffect(() => {
-    function handleResize() {
-      const width = window.innerWidth;
-      if (width >= 1920) {
-        setCalendarSize({
-          cx: 17,
-          cy: 17,
-          cr: 9,
-        });
-      } else if (width >= 1600) {
-        setCalendarSize({
-          cx: 14,
-          cy: 14,
-          cr: 5,
-        });
-      } else if (width >= 1366) {
-        setCalendarSize({
-          cx: 11,
-          cy: 11,
-          cr: 5,
-        });
-      } else if (width >= 768) {
-        setCalendarSize({
-          cx: 6,
-          cy: 6,
-          cr: 5,
-        });
-      }
-    }
+    useEffect(() => {
+        function handleResize() {
+            const width = window.innerWidth;
+            if (width >= 1920) {
+                setCalendarSize({
+                    cx: 17,
+                    cy: 17,
+                    cr: 9,
+                });
+            } else if (width >= 1600) {
+                setCalendarSize({
+                    cx: 14,
+                    cy: 14,
+                    cr: 5,
+                });
+            } else if (width >= 1366) {
+                setCalendarSize({
+                    cx: 11,
+                    cy: 11,
+                    cr: 5,
+                });
+            } else if (width >= 768) {
+                setCalendarSize({
+                    cx: 6,
+                    cy: 6,
+                    cr: 5,
+                });
+            }
+        }
 
-    window.addEventListener("resize", handleResize);
-    handleResize();
+        window.addEventListener("resize", handleResize);
+        handleResize();
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
 
-  }, []);
+    }, []);
 
-  const userId = "1";
-  const dispatch = useDispatch();
+    const userId = "1";
+    const dispatch = useDispatch();
 
-  const userContributionList = useSelector((state) => state.userContributions);
-  const { loading, error, contributions } = userContributionList;
+    const userContributionList = useSelector((state) => state.userContributions);
+    const { loading, error, contributions } = userContributionList;
 
-  const debounceRef = useRef(null);
+    const debounceRef = useRef(null);
 
-  useEffect(() => {
-    clearTimeout(debounceRef.current);
-    debounceRef.current = setTimeout(() => {
-      dispatch(getUserContributions(userId));
-    }, 200);
-  }, [])
+    useEffect(() => {
+        clearTimeout(debounceRef.current);
+        debounceRef.current = setTimeout(() => {
+            dispatch(getUserContributions(userId));
+        }, 200);
+    }, [])
 
-  return (
-    <>
-      <Card className="mb-4">
-        <Title>Newsletter revenue over time (USD)</Title>
-        <AreaChart
-          className="h-72 mt-4"
-          data={chartdata}
-          index="date"
-          categories={["SemiAnalysis", "The Pragmatic Engineer"]}
-          colors={["indigo", "cyan"]}
-          valueFormatter={dataFormatter}
-        />
-      </Card>
-      <div>
-        {loading ? (
-          <p> Loading... </p>
-        ) : error ? (
-          <MessageBox variant="danger">{error}</MessageBox>
-        ) : (
-          <Card>
-            <Title>Total {contributions.totalUserCommits} contributions</Title>
-            <div className="dark">
-              <ContributionCalendar
-                start={contributions.startDate}
-                end={contributions.endDate}
-                theme={customTheme}
-                textColor="#4b5364"
-                data={contributions.userCommits}
-                daysOfTheWeek={["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]}
-                includeBoundary={false}
-                startsOnSunday={true}
-                cx={calendarSize.cx}
-                cy={calendarSize.cy}
-                cr={calendarSize.cr}
-                // theme="purquoise"
-                onCellClick={(_, data) => console.log(data)}
-                scroll={false}
-              />
+    const compareCommits = useSelector((state) => state.compareCommits);
+    const { commits } = compareCommits;
+
+    const debounceChartRef = useRef(null);
+
+    useEffect(() => {
+        clearTimeout(debounceChartRef.current);
+        debounceChartRef.current = setTimeout(() => {
+            dispatch(getCompareCommits(userId));
+        }, 200);
+    }, []);
+
+    return (
+        <>
+            <div>
+                {loading ? (
+                    <p> Loading... </p>
+                ) : error ? (
+                    <MessageBox variant="danger">{error}</MessageBox>
+                ) : (
+                    <>
+                        <Card className="mb-4">
+                            <Title>Total {contributions.totalUserCommits} contributions</Title>
+                            <div className="dark">
+                                <ContributionCalendar
+                                    start={contributions.startDate}
+                                    end={contributions.endDate}
+                                    theme={customTheme}
+                                    textColor="#4b5364"
+                                    data={contributions.userCommits}
+                                    daysOfTheWeek={["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]}
+                                    includeBoundary={false}
+                                    startsOnSunday={true}
+                                    cx={calendarSize.cx}
+                                    cy={calendarSize.cy}
+                                    cr={calendarSize.cr}
+                                    // theme="purquoise"
+                                    onCellClick={(_, data) => console.log(data)}
+                                    scroll={false}
+                                />
+                            </div>
+                        </Card>
+
+                        <Card className="mb-4">
+                            <Title>Your commits in 2 weeks</Title>
+                            <AreaChart
+                                className="h-72 mt-4"
+                                data={commits}
+                                index="date"
+                                categories={["Weekly Commit", "Previous Weekly Commit"]}
+                                colors={["cyan", "indigo"]}
+                            />
+                        </Card>
+                    </>
+                )}
+
             </div>
-          </Card>
-        )}
-
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default AreaChartComponent;
