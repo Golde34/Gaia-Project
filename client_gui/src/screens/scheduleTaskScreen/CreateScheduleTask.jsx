69110f3ae59a3718cc5141c7cfc360f26b9e083c
Golde34 <nguyendongducviet2001@gmail.com>
2025-03-25 18:38:45 +0700

    add startHour and endHour and description when create schedule task

--- client_gui/src/screens/scheduleTaskScreen/CreateScheduleTask.jsx
+++ client_gui/src/screens/scheduleTaskScreen/CreateScheduleTask.jsx
@@ -1,5 +1,5 @@
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from "@headlessui/react";
-import { Button, Col, Grid, TextInput } from "@tremor/react";
+import { Button, Col, Grid, Textarea, TextInput } from "@tremor/react";
 import { Fragment, useState } from "react";
 import CheckBoxIcon from "../../components/icons/CheckboxIcon";
 import { pushPriority, pushRepeat } from "../../kernels/utils/field-utils";
@@ -18,6 +18,7 @@ export const CreateScheduleTaskDialog = (props) => {
 
     const [scheduleTask] = useState({});
     const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
     const [duration, setDuration] = useState(0);
     const [startHour, setStartHour] = useState(new Date());
     const [endHour, setEndHour] = useState(new Date());
@@ -35,12 +36,13 @@ export const CreateScheduleTaskDialog = (props) => {
     const [isSaturday, setIsSaturday] = useState(false);
     const [isSunday, setIsSunday] = useState(false);
 
-    const setObjectTask = (title, duration, startHour, endHour,
+    const setObjectTask = (title, duration, description, startHour, endHour,
         isHighPriority, isMediumPriority, isLowPriority, isStarPriority,
         isMonday, isTuesday, isWednesday, isThursday, isFriday, isSaturday, isSunday) => {
         scheduleTask.userId = userId;
         scheduleTask.title = title;
         scheduleTask.duration = duration;
+        scheduleTask.description = description;
         scheduleTask.startHour = startHour;
         scheduleTask.endHour = endHour;
         scheduleTask.priority = pushPriority(isHighPriority, isMediumPriority, isLowPriority, isStarPriority);
@@ -102,6 +104,17 @@ export const CreateScheduleTaskDialog = (props) => {
                                             onChange={(e) => setTitle(e.target.value)}
                                             className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                                             placeholder="Task Title"
+                                        />
+                                    </div>
+
+                                    <div className="mt-4">
+                                        <label htmlFor="task-description" className="block text-md font-medium text-gray-700 mb-3">Description</label>
+                                        <Textarea
+                                            id="task-description"
+                                            value={description}
+                                            onChange={(e) => setDescription(e.target.value)}
+                                            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                            placeholder="Task Description"
                                         />
                                     </div>
 
@@ -365,7 +378,8 @@ export const CreateScheduleTaskDialog = (props) => {
                                             type="button"
                                             className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                                             onClick={() => {
-                                                setObjectTask(title, duration, isHighPriority, isMediumPriority, isLowPriority, isStarPriority,
+                                                setObjectTask(title, duration, description, startHour, endHour, 
+                                                    isHighPriority, isMediumPriority, isLowPriority, isStarPriority,
                                                     isMonday, isTuesday, isWednesday, isThursday, isFriday, isSaturday, isSunday);
                                                 closeModal();
                                             }}
