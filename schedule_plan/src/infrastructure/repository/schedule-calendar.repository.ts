2b805929514889a30be73ad9ec7209f8a365e501
Golde34 <nguyendongducviet2001@gmail.com>
2025-04-09 09:25:36 +0700

    add schedule calendar usecase

--- schedule_plan/src/infrastructure/repository/schedule-calendar.repository.ts
+++ schedule_plan/src/infrastructure/repository/schedule-calendar.repository.ts
@@ -1,3 +1,4 @@
+import { UpdateWriteOpResult } from "mongoose";
 import { IScheduleCalendarEntity, ScheduleCalendarEntity } from "../entities/schedule-calendar.entity";
 
 class ScheduleCalendarRepository {
@@ -10,6 +11,10 @@ class ScheduleCalendarRepository {
     async findScheduleCalendarByUserId(userId: number): Promise<IScheduleCalendarEntity | null> {
         return await ScheduleCalendarEntity.findOne({ userId: userId });
     }
+
+    async updateScheduleCalendar(userId: number, scheduleCalendar: any): Promise<UpdateWriteOpResult> {
+        return await ScheduleCalendarEntity.updateOne({ userId: userId }, scheduleCalendar);
+    }        
 }
 
 export const scheduleCalendarRepository = new ScheduleCalendarRepository();
