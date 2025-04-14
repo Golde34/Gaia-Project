74bc069dce612669311ff67126423dc28627f7fe
Golde34 <nguyendongducviet2001@gmail.com>
2025-04-14 22:33:35 +0700

    add done tasks component in Right Column

--- client_gui/src/components/subComponents/Item.jsx
+++ client_gui/src/components/subComponents/Item.jsx
@@ -1,26 +1,8 @@
-import React, { useState } from "react";
-import {
-  BadgeDelta,
-  Button,
-  Card,
-  DonutChart,
-  Flex,
-  TabGroup,
-  Tab,
-  TabList,
-  Bold,
-  Divider,
-  List,
-  ListItem,
-  Metric,
-  Text,
-  Title,
-} from "@tremor/react";
-import {
-  ArrowRightIcon,
-  ChartPieIcon,
-  ViewListIcon,
-} from "@heroicons/react/outline";
+import { ArrowRightIcon, ChartPieIcon, ViewListIcon } from "@heroicons/react/outline";
+import { Bold, Button, Card, Divider, DonutChart, Flex, List, ListItem, Metric, Tab, TabGroup, TabList, Text, Title } from "@tremor/react";
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getDoneTasks } from "../../api/store/actions/task_manager/task.actions";
 
 const stocks = [
   {
@@ -60,68 +42,96 @@ const dataFormatter = (number) => {
 };
 
 const SalesItem = () => {
+  const userId = "1";
+  const dispatch = useDispatch();
+
+  const { loading, error, doneTasks } = useSelector((state) => state.doneTasks);
+  const didGetDoneTaskRef = useRef();
+  useEffect(() => {
+    if (didGetDoneTaskRef.current) return;
+    dispatch(getDoneTasks(userId));
+    didGetDoneTaskRef.current = true;
+  }, [dispatch]);
+
+
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
-    <Card className="max-w-full mx-auto">
-      <Flex className="space-x-8 flex-col lg:flex-row">
-        <Title className="">Overview</Title>
-        <TabGroup index={selectedIndex} onIndexChange={setSelectedIndex}>
-          <TabList variant="solid">
-            <Tab icon={ChartPieIcon}>Chart</Tab>
-            <Tab icon={ViewListIcon}>List</Tab>
-          </TabList>
-        </TabGroup>
-      </Flex>
-      <Text className="mt-8">Portfolio Value</Text>
-      <Metric>$ 25,465</Metric>
-      <Divider />
-      <Text className="mt-8">
-        <Bold>Asset Allocation</Bold>
-      </Text>
-      <Text>1 Asset class - 5 Holdings</Text>
-      {selectedIndex === 0 ? (
-        <DonutChart
-          data={stocks}
-          valueFormatter={dataFormatter}
-          showAnimation={false}
-          category="value"
-          index="name"
-          className="mt-6"
-        />
+    <>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <MessageBox message={error} />
       ) : (
-        <>
-          <Flex className="mt-8" justifyContent="between">
-            <Text className="truncate">
-              <Bold>Stocks</Bold>
-            </Text>
-            <Text>Since transaction</Text>
-          </Flex>
-          <List className="mt-4">
-            {stocks.map((stock) => (
-              <ListItem key={stock.name}>
-                <Text>{stock.name}</Text>
-                <Flex className="space-x-2" justifyContent="end">
-                  <Text>
-                    $ {Intl.NumberFormat("us").format(stock.value).toString()}
-                  </Text>
-                </Flex>
-              </ListItem>
-            ))}
-          </List>
-        </>
-      )}
-      <Flex className="mt-6 pt-4 border-t">
-        <Button
-          size="xs"
-          variant="light"
-          icon={ArrowRightIcon}
-          iconPosition="right"
-        >
-          View more
-        </Button>
-      </Flex>
-    </Card>
+        doneTasks.length === 0 ? (
+          <div></div>
+        ) : (
+          <div>
+            <Card className="max-w-full mx-auto">
+              <Flex className="space-x-8 flex-col lg:flex-row">
+                <Title className="">Overview</Title>
+                <TabGroup index={selectedIndex} onIndexChange={setSelectedIndex}>
+                  <TabList variant="solid">
+                    <Tab icon={ChartPieIcon}>Chart</Tab>
+                    <Tab icon={ViewListIcon}>List</Tab>
+                  </TabList>
+                </TabGroup>
+              </Flex>
+              <Text className="mt-8">Total Done Tasks</Text>
+              <Metric>{doneTasks.length}</Metric>
+              <Divider />
+              <Text className="mt-8">
+                <Bold>Asset Allocation</Bold>
+              </Text>
+              <Text>1 Asset class - 5 Holdings</Text>
+              {selectedIndex === 0 ? (
+                <DonutChart
+                  data={stocks}
+                  valueFormatter={dataFormatter}
+                  showAnimation={false}
+                  category="value"
+                  index="name"
+                  className="mt-6"
+                />
+              ) : (
+                <>
+                  <Flex className="mt-8" justifyContent="between">
+                    <Text className="truncate">
+                      <Bold>Stocks</Bold>
+                    </Text>
+                    <Text>Since transaction</Text>
+                  </Flex>
+                  <List className="mt-4">
+                    {stocks.map((stock) => (
+                      <ListItem key={stock.name}>
+                        <Text>{stock.name}</Text>
+                        <Flex className="space-x-2" justifyContent="end">
+                          <Text>
+                            $ {Intl.NumberFormat("us").format(stock.value).toString()}
+                          </Text>
+                        </Flex>
+                      </ListItem>
+                    ))}
+                  </List>
+                </>
+              )}
+              <Flex className="mt-6 pt-4 border-t">
+                <Button
+                  size="xs"
+                  variant="light"
+                  icon={ArrowRightIcon}
+                  iconPosition="right"
+                >
+                  View more
+                </Button>
+              </Flex>
+            </Card>
+          </div>
+        )
+      )
+      }
+
+    </>
   );
 };
 
--- client_gui/src/views/RightColumn.jsx
+++ client_gui/src/views/RightColumn.jsx
@@ -1,13 +1,11 @@
 import Item from "../components/subComponents/Item";
 import CompareCommitChart from "../components/subComponents/CompareCommitsChart";
-import ScoreList from "../components/subComponents/ScoreList";
 
 const RightColumn = () => {
 	return (
 		<div className="w-full p-2">
 			<CompareCommitChart />
 			<Item />
-			{/* <ScoreList /> */}
 		</div>
 	);
 };
