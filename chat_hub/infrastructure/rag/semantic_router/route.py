02181cb6e68354d2707ce137eebec73b929295d0
Golde34 <nguyendongducviet2001@gmail.com>
2025-04-18 23:37:20 +0700

    make chat hub written in golang

--- chat_hub/infrastructure/rag/semantic_router/route.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-
-class Route():
-    def __init__(self, name: str = None, samples: List = [], service=None):
-        self.name = name
-        self.samples = samples
-        self.service = service
