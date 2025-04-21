581dab430af4beaf88b58c0233c04eea4540b0bb
Golde34 <nguyendongducviet2001@gmail.com>
2025-04-21 12:48:34 +0700

    add llm core request object

--- chat_hub/infrastructure/client/llm_core_adapter.go
+++ chat_hub/infrastructure/client/llm_core_adapter.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	request_dtos "chat_hub/core/domain/dtos/request"
+	"chat_hub/core/domain/enums"
 	"chat_hub/infrastructure/client/base"
 	"chat_hub/kernel/utils"
 	"log"
@@ -12,20 +14,21 @@ func NewLLMCoreAdapter() *LLMCoreAdapter {
 	return &LLMCoreAdapter{}
 }
 
-func (adapter *LLMCoreAdapter) UserPrompt(userId, message string) (string, error) {
-	log.Println("Sending user prompt to LLMCoreAdapter for user ID: " + message)
-	userPrompURL := base.LLMCoreServiceURL + "/chat-hub/user-prompt/" + userId
+func (adapter *LLMCoreAdapter) UserPrompt(input request_dtos.LLMQueryRequestDTO) (string, error) {
+	userPrompURL := base.LLMCoreServiceURL + "/chat"
 	headers := utils.BuildDefaultHeaders()
-	bodyResult, err := utils.BaseAPI(userPrompURL, "GET", nil, headers)
+	bodyResult, err := utils.BaseAPI(userPrompURL, enums.POST, input, headers)
 	if err != nil {
 		return "Cannot connect with Gaia Bot, try later", err
 	}
 
+	log.Println("Response from LLMCoreAdapter: ", bodyResult)
 	bodyeResultMap, ok := bodyResult.(map[string]interface{})
 	if !ok {
 		return "Internal System Error", nil
 	}
 
+	log.Println("Response map from LLMCoreAdapter: ", bodyeResultMap)
 	data, exists := bodyeResultMap["response"].(string)
 	if !exists || data == "" {
 		return "Internal System Error", err
