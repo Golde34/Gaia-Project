47cece601d2d71114a83645819a1dea5a816c62c
Golde34 <nguyendongducviet2001@gmail.com>
2025-04-23 10:18:37 +0700

    handle command type from AIC

--- chat_hub/core/services/chat/chat_service.go
+++ chat_hub/core/services/chat/chat_service.go
@@ -6,7 +6,7 @@ import (
 	"log"
 )
 
-type ChatService struct {}
+type ChatService struct{}
 
 func NewChatService() *ChatService {
 	return &ChatService{}
@@ -27,11 +27,29 @@ func (s *ChatService) HandleChatMessage(userId string, message string) (string, 
 	var input request_dtos.LLMQueryRequestDTO
 	input.UserId = userId
 	input.ModelName = userModel.ModelName
-	input.Query= message
+	input.Query = message
 	chatResponse, err := client.NewLLMCoreAdapter().UserPrompt(input)
 	if err != nil {
 		log.Println("Error sending message to LLMCoreAdapter: " + err.Error())
 		return "", err
 	}
-	return chatResponse, nil	
+	
+	go handleChatResponse(chatResponse, userId)
+	
+	data, exists := chatResponse["response"].(string)
+	if !exists || data == "" {
+		return "Internal System Error", err
+	}
+
+	return data, nil
+}
+
+func handleChatResponse(chatResponse map[string]interface{}, userId string) {
+	log.Println("Handling chat response for user " + userId)
+	if chatResponse["type"] == "chitchat" {
+		log.Println("Chitchat response for user " + userId)
+	}
+	if chatResponse["type"] == "create_task" {
+		log.Println("Create task response for user " + userId)
+	}	
 }
