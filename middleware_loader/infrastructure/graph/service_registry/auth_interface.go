059ca89f589df9bbcec4cb7bef86c4efe3ac7e8a
Golde34 <nguyendongducviet2001@gmail.com>
2025-04-30 07:37:56 +0700

    upgrade sign in function

--- middleware_loader/infrastructure/graph/service_registry/auth_interface.go
+++ middleware_loader/infrastructure/graph/service_registry/auth_interface.go
@@ -3,11 +3,12 @@ package service_registry
 import (
 	"context"
 
+	response_dtos "middleware_loader/core/domain/dtos/response"
 	"middleware_loader/infrastructure/graph/model"
 )
 
 type AuthService interface {
-	Signin(ctx context.Context, input model.SigninInput) (model.AuthTokenResponse, error)
+	Signin(ctx context.Context, input model.SigninInput) (model.AuthTokenResponse, response_dtos.AuthTokenResponseDTO, error)
 	GaiaAutoSignin(ctx context.Context, input model.SigninInput) (model.AuthTokenResponse, error)
 	CheckToken(ctx context.Context, input model.TokenInput) (model.TokenResponse, error)
 }
