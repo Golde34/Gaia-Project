70dce5e60d407ae6dc90a0212a8b31a1b908dbae
Golde34 <nguyendongducviet2001@gmail.com>
2025-05-27 18:52:26 +0700

    add daily routine calendar creatioon function

--- ai_core/core/service/onboarding_service.py
+++ ai_core/core/service/onboarding_service.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from core.domain.request.query_request import SystemRequest
 from core.domain.response.model_output_schema import DailyRoutineSchema
@@ -17,17 +18,23 @@ def register_task(query: SystemRequest) -> dict:
         user_daily_entries (dict):  
     """
     try:
-        prompt = ONBOARDING_PROMPT.format(query=query.query)  
+        prompt = ONBOARDING_PROMPT.format(query=query.query)
         print("Onboarding Prompt:", prompt)
         response = llm_models.get_model_generate_content(
-            "gemini-2.0-flash")(prompt=prompt, 
+            "gemini-2.0-flash")(prompt=prompt,
                                 model_name="gemini-2.0-flash",
                                 )
-        print("Onboarding Response:", response)
-        print("Onboarding response type:", type(response))
+        json_str = clean_json_string(response)
+        data_dict = json.loads(json_str)
+        schedule_dto = DailyRoutineSchema.parse_obj(data_dict)
         result = {
-            "response": response
-        } 
+            "response": schedule_dto 
+        }
         return result
     except Exception as e:
         raise e
+
+def clean_json_string(raw_str: str) -> str:
+    cleaned = re.sub(r"^```json\s*|\s*```$", "",
+                    raw_str, flags=re.MULTILINE).strip()
+    return cleaned
