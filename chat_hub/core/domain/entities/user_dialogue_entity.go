a46008eab525ffbed97f303daf6891f5a63abec9
Golde34 <nguyendongducviet2001@gmail.com>
2025-06-20 22:12:31 +0700

    create entity services in CH

--- chat_hub/core/domain/entities/user_dialogue_entity.go
+++ chat_hub/core/domain/entities/user_dialogue_entity.go
@@ -1,14 +1,12 @@
 package entity
 
 type UserDialogueEntity struct {
-	ID          string `json:"id"`
-	UserID      string `json:"user_id"`
-	DialogueID  string `json:"dialogue_id"`
-	CreatedAt   string `json:"created_at"`
-	UpdatedAt   string `json:"updated_at"`
-	DialogueName string `json:"dialogue_name"`
-	DialogueType string `json:"dialogue_type"`
-	DialogueStatus string `json:"dialogue_status"`
-	DialogueMetadata string `json:"dialogue_metadata"`
+	ID             string `json:"id"`
+	UserID         string `json:"user_id"`
+	DialogueName   string `json:"dialogue_name"`
+	DialogueType   string `json:"dialogue_type"`
+	DialogueStatus bool   `json:"dialogue_status"`
+	Metadata       string `json:"metadata"`
+	CreatedAt      string `json:"created_at"`
+	UpdatedAt      string `json:"updated_at"`
 }
-
---
+++ chat_hub/core/domain/enums/enums.go
@@ -0,0 +1,35 @@
+package enums
+
+const (
+	ChatMessage = "chat_message"
+)
+
+var (
+	FullBody = "FULL_BODY"
+	OnlyData = "ONLY_DATA"
+	OnlyStatus = "ONLY_STATUS"
+	OnlyMessage = "ONLY_MESSAGE"
+)
+
+var (
+	POST = "POST"
+	GET  = "GET"
+	PUT  = "PUT"
+	DELETE = "DELETE"
+	HEAD = "HEAD"
+)
+
+var (
+	GaiaIntroductionDialogue = "Gaia Introduction"
+	GaiaIntroductionDialogueType = "introduction"
+
+	RegisterCalendarDialogue = "Task Register Calendar"
+	RegisterCalendarDialogueType = "register_calendar"
+
+	ChatDialogueType = "chat"
+)
+
+var (
+	ACTIVE = true 
+	INACTIVE = false 
+)
