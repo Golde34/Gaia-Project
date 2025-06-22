eed389b91ca84d0322b97cb333e14230aba08a9a
Golde34 <nguyendongducviet2001@gmail.com>
2025-06-22 09:11:41 +0700

    add cha_hub database script

---
+++ docker_manifest/database_script/chat_hub/1.0.0/user_message.sql
@@ -0,0 +1,22 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE public.user_messages (
+    id UUID DEFAULT gen_random_uuid(),
+    user_id BIGINT NOT NULL,
+    dialogue_id UUID NOT NULL,
+    message_type VARCHAR(50) NOT NULL,
+    order_number INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    metadata TEXT NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NULL,
+    updated_at TIMESTAMP WITHOUT TIME ZONE NULL, 
+    PRIMARY KEY (id, created_at)
+)
+
+CREATE INDEX idx_user_messages_user_id
+    ON public.user_messages (user_id);
+
+CREATE INDEX idx_user_messages_dialogue_id
+    ON public.user_messages (dialogue_id);
+
+SELECT create_hypertable('user_messages', 'created_at', if_not_exists => TRUE);
