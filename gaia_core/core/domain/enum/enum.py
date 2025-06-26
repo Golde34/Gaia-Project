39dcd327e79025bb874271f65e29df92502597fe
Golde34 <nguyendongducviet2001@gmail.com>
2025-06-26 23:12:05 +0700

    add rag service in GAC

---
+++ gaia_core/core/domain/enum/enum.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class ModelMode(Enum):
+    LOCAL = "local"
+    VLLM = "vllm"
+    CLOUD = "cloud"
+
+class SemanticRoute(Enum):
+    GAIA_INTRODUCTION = "gaia_introduction"
+    CHITCHAT = "chitchat"
+
+class TaskServiceRoute(Enum):
+    CHITCHAT = "chitchat"
+    CREATE_TASK = "create_task"
