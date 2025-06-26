959cf1956d4ab2a71b177cc6edf346dd7d7c6c65
Golde34 <nguyendongducviet2001@gmail.com>
2025-06-26 23:39:17 +0700

    add chat usecase flow

--- gaia_core/core/domain/enum/enum.py
+++ gaia_core/core/domain/enum/enum.py
@@ -1,6 +1,11 @@
 from enum import Enum
 
 
+class ChatType(Enum):
+    ABILITIES = "abilities"
+    GAIA_INTRODUCTION = "gaia_introduction"
+    REGISTER_SCHEDULE_CALENDAR = "register_schedule_calendar" 
+
 class ModelMode(Enum):
     LOCAL = "local"
     VLLM = "vllm"
@@ -9,7 +14,8 @@ class ModelMode(Enum):
 class SemanticRoute(Enum):
     GAIA_INTRODUCTION = "gaia_introduction"
     CHITCHAT = "chitchat"
+    REGISTER_SCHEDULE_CALENDAR = "register_schedule_calendar"
 
-class TaskServiceRoute(Enum):
+class Abiliti(Enum):
     CHITCHAT = "chitchat"
     CREATE_TASK = "create_task"
---
+++ gaia_core/core/usecase/chat.py
@@ -0,0 +1,14 @@
+from core.domain.request.query_request import QueryRequest
+
+
+async def chat(query: QueryRequest, chat_type: str):
+    # _check_redis()
+    # new_prompt = _reflection()
+    # function_prompt = ABILITIES_HANDLER.get(new_prompt) 
+    # llm_response = _llm()
+    # await _update_recent_history()
+    # await _update_recursive_history()
+    # await _update_long_term_memory()
+    # await _update_redis()
+    # return llm_response
+    pass
