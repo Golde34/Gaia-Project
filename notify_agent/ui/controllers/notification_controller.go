c1030043f501f5a93fde5115212c8012595ba4ec
Golde34 <nguyendongducviet2001@gmail.com>
2025-09-07 21:06:13 +0700

    NA add base response of get all notifications api

--- notify_agent/ui/controllers/notification_controller.go
+++ notify_agent/ui/controllers/notification_controller.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	base_dtos "notify_agent/core/domain/dtos/base"
 	services "notify_agent/core/services/business"
 
 	"github.com/go-chi/chi"
@@ -18,8 +19,16 @@ func GetAllNotificationsByUserId(w http.ResponseWriter, r *http.Request, notific
 		return
 	}
 
+	response := base_dtos.ErrorResponse{
+		Status:        "success",
+		StatusMessage: "Get notifications successfully",
+		ErrorCode:     200,
+		ErrorMessage:  "Success",
+		Data:          notifications,
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(notifications); err != nil {
+	if err := json.NewEncoder(w).Encode(response); err != nil {
 		log.Printf("Error encoding response: %v", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
