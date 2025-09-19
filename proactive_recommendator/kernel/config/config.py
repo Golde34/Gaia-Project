d2e7c099a1cc374508c83572c8720995ba93976d
Golde34 <nguyendongducviet2001@gmail.com>
2025-09-19 18:19:17 +0700

    PR add redis

--- proactive_recommendator/kernel/config/config.py
+++ proactive_recommendator/kernel/config/config.py
@@ -8,3 +8,7 @@ class Config:
     NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
     NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
     NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password")
+
+    REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+    REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
+    REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', 'bitnami')
