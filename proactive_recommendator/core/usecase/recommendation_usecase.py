d5505d7b01cd1d84b9980934fc580a8e54bb9852
Golde34 <nguyendongducviet2001@gmail.com>
2025-09-28 20:05:36 +0700

    init recommend flow

--- proactive_recommendator/core/usecase/recommendation_usecase.py
+++ proactive_recommendator/core/usecase/recommendation_usecase.py
@@ -4,13 +4,18 @@ from core.usecase import command_label_usecase, user_information_usecase
 
 
 async def recommend(body: RecommendationRequest) -> str:
-    ## Validate user information
-    graph_user_id = user_information_usecase.get_user_information(body.user_id) 
-    ## Get the main label
-    _, first, rest = await command_label_usecase.rank_labels_by_relevance(body.query)
-    ## Get the data from graphDB 
-    # Generate GraphDB query from LLM Model 
-    
-    data = {"first": first, "rest": rest} 
-    return return_success_response("get the label when analyze query", data) 
-    ## ...
+    try:
+        ## Validate user information
+        user_information = user_information_usecase.get_user_information(body.user_id) 
+        ## Get the main label
+        results, first, rest = await command_label_usecase.rank_labels_by_relevance(body.query)
+        recommendation_information = await user_information_usecase.validate_labels_information(results)
+        ## Get the data from graphDB 
+        # Generate GraphDB query from LLM Model
+        # After get the labels's relationships, usering PPR to compare
+        ## Generate response using LLM model + do something before user needs, like, generate calendar, ... 
+        data = {"first": first, "rest": rest} 
+        return return_success_response("get the label when analyze query", data) 
+        ## ...
+    except Exception as e:
+        return "Error: " + e 
--- proactive_recommendator/core/usecase/user_information_usecase.py
+++ proactive_recommendator/core/usecase/user_information_usecase.py
@@ -1,4 +1,5 @@
 import json
+from typing import List
 
 from core.domain.enums.redis_enum import RedisEnum
 from core.domain.response.base_response import BaseResponse
@@ -34,6 +35,10 @@ async def create_user_information(user_id: int, redis_key: str):
         dict(result)), ttl=60*60*24)
     return result
 
+async def validate_labels_information(results: List[str]):
+    ## for example: labels is create task, list task, user health -> pending when create task successfully, then call api check list task, user health
+    # label is list task, user health, call api check list task, user health immediately
+    pass
 
 def get_user_task_list(query: str):
     pass
