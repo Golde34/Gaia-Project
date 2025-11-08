5e080fa8baec44864e4c50309b7cbb1d5d4422de
Golde34 <nguyendongducviet2001@gmail.com>
2025-11-08 21:36:47 +0700

    GCJ remove register schedule calendar consumer

--- gaia_cron_jobs/domain/kafka_topic.go
+++ gaia_cron_jobs/domain/kafka_topic.go
@@ -4,9 +4,6 @@ const (
 	// KafkaTopicCronJob is the topic for cron job
 	FullSyncGithubCommitTopic = "contribution-tracker.full-sync-github-commit.topic"
 	SyncGithubCommitTopic = "contribution-tracker.github-commit.topic"
-
-	AIGenerateScheduleCalendar = "ai-core.generate-calendar-schedule.topic"
-	AIRegisterScheduleCalendar = "ai-core.register-calendar-schedule.topic"
 )
 
 const (
