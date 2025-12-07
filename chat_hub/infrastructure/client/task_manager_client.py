71f65c0a453c97d0ec9fddd594d8793dcb7363a1
Golde34 <nguyendongducviet2001@gmail.com>
2025-12-07 23:37:15 +0700

    CH add task manager client

---
+++ chat_hub/infrastructure/client/task_manager_client.py
@@ -0,0 +1,32 @@
+from chat_hub.core.domain.response.model_output_schema import DailyRoutineSchema
+from chat_hub.kernel.config import config
+from chat_hub.kernel.utils import aiohttp_utils, build_header
+
+
+class TaskManagerClient:
+    """
+    Client for interacting with the Task Manager Service.
+    This service provides functionalities related to task management.
+    """
+    def __init__(self):
+        self.base_url = config.TASK_MANAGER_SERVICE_URL
+
+    async def create_personal_task(self, task: dict) -> dict:
+        try:
+            endpoint = f"{self.base_url}/task/create"
+            headers = build_header.build_default_headers()
+            result = await aiohttp_utils.post(
+                endpoint=endpoint,
+                payload=task,
+                header=headers
+            )
+            if not result:
+                print("No response from Task Manager Service for create_task.")
+                return {}
+
+            return result["data"]
+        except Exception as e:
+            print(f"Error in SchedulePlanClient.create_or_update_time_bubble_configs: {e}")
+            return {} 
+    
+task_manager_client = TaskManagerClient()
