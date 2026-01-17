8b62d9d08e260dd0a2e5103d551b19488e3ce6ea
Golde34 <nguyendongducviet2001@gmail.com>
2026-01-17 16:19:34 +0700

    ML fix lua script

--- middleware_loader/kernel/resource/check_and_decrease_quota.lua
+++ middleware_loader/kernel/resource/check_and_decrease_quota.lua
@@ -8,21 +8,16 @@ local key = KEYS[1]
 local maxCount = tonumber(ARGV[1])
 local ttl = tonumber(ARGV[2])
 
--- Get current value
 local current = redis.call('GET', key)
 
 if current == false then
-    -- Key doesn't exist, initialize with maxCount - 1
-    redis.call('SETEX', key, 86400, maxCount - 1)  -- TTL = 24 hours
+    redis.call('SETEX', key, ttl, maxCount - 1)
     return maxCount - 1
 else
-    -- Key exists, check if we can decrease
     current = tonumber(current)
     if current <= 0 then
-        -- Quota exceeded
         return -1
     else
-        -- Decrease by 1
         local remaining = redis.call('DECR', key)
         return remaining
     end
