53b77472f639b3e3c412e44f26a4f6349cb0d376
Golde34 <nguyendongducviet2001@gmail.com>
2026-01-24 14:29:46 +0700

    TM init core kernel

---
+++ person_task_manager/new_server/core/domain/enums/enums.go
@@ -0,0 +1,21 @@
+package enums
+
+var (
+	FullBody = "FULL_BODY"
+	OnlyData = "ONLY_DATA"
+	OnlyStatus = "ONLY_STATUS"
+	OnlyMessage = "ONLY_MESSAGE"
+)
+
+var (
+	POST = "POST"
+	GET  = "GET"
+	PUT  = "PUT"
+	DELETE = "DELETE"
+	HEAD = "HEAD"
+)
+
+var (
+	ACTIVE = true 
+	INACTIVE = false 
+)
