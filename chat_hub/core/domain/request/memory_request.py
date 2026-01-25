63062d9c59c4c09e8b6bd5eedb6d14b825f92206
Golde34 <nguyendongducviet2001@gmail.com>
2026-01-25 14:06:48 +0700

    CH clean code

--- chat_hub/core/domain/request/memory_request.py
+++ chat_hub/core/domain/request/memory_request.py
@@ -6,8 +6,14 @@ class MemoryRequest(BaseModel):
     dialogue_id: str
     is_change_title: bool = False
 
-class MemoryQueryDto(BaseModel):
+class MemorySelectionToolDto(BaseModel):
     query: str
     reflected_query: str
     last_bot_message: str
+    used_tools: list[str]
+
+class MemoryDto(BaseModel):
+    recent_history: str
+    recursive_summary: str
+    longterm_memory: str
     used_tools: list[str]
