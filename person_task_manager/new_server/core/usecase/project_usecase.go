4d863e9be4d10fc85b4f2027420879ace239e622
Golde34 <nguyendongducviet2001@gmail.com>
2026-02-09 07:57:37 +0700

    TM route example

--- person_task_manager/new_server/core/usecase/project_usecase.go
+++ person_task_manager/new_server/core/usecase/project_usecase.go
@@ -11,3 +11,13 @@ func (pu *ProjectUsecase) CreateProject(name string, description string) error {
 	// Implement the logic to create a project
 	return nil
 }
+
+func (pu *ProjectUsecase) GetProjectByID(id string) (map[string]string, error) {
+	// Implement the logic to get a project by ID
+	project := map[string]string{
+		"id":          id,
+		"name":        "Sample Project",
+		"description": "This is a sample project description.",
+	}
+	return project, nil
+}
