df112043aab0da5256f6f95a015c3b2b75667d3e
Golde34 <nguyendongducviet2001@gmail.com>
2026-02-23 07:44:42 +0700

    TM add tag in create project request

--- person_task_manager/new_server/cmd/route/route.go
+++ person_task_manager/new_server/cmd/route/route.go
@@ -5,11 +5,12 @@ import (
 
 	"github.com/go-chi/chi"
 
-	project_router "personal_task_manager/ui/router"
+	br "personal_task_manager/ui/router"
 )
 
 func Setup(router *chi.Mux, db *sql.DB) {
 	router.Group(func(r chi.Router) {
-		project_router.NewProjectRouter(router, db)
+		br.NewProjectRouter(router, db)
+		br.NewGroupTaskRouter(router, db)
 	})
 }
